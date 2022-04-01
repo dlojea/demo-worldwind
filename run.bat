7d35081723762516c97a1ec43e951ef85560852d
dlojea <dlojea@esei.uvigo.es>
2022-04-01 12:57:55 +0200

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,7 +1,5 @@
 @echo off
-REM Default to the ApplicationTemplate example if a class name is not provided
 IF "%1"=="" (SET WWDEMO=Demo) ELSE (SET WWDEMO=%1)
 
-REM Run a WorldWind Demo
 @echo Running %WWDEMO%
 java -cp demo.jar;lib/worldwind.jar;lib/worldwindx.jar;lib/gdal.jar;lib/jogl-all.jar;lib/gluegen-rt.jar;lib/java-json.jar %WWDEMO%
